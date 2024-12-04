77f03a6678dd6bcbba57da78c4d6d1789d3a7e37
LimeFrogg <bellamones@icloud.com>
2024-12-03 20:20:11 -0500

    Question 3

--- java.js
+++ java.js
@@ -5,3 +5,10 @@ function greetUser(Isabella) {
 document.getElementById('changeTextBtn').addEventListener('click', function() {
     document.getElementById('message').innerText = 'Hello, World!';
 });
+
+document.getElementById('addNumbersBtn').addEventListener('click', function() {
+    let num1 = parseFloat(document.getElementById('5').value);
+    let num2 = parseFloat(document.getElementById('6').value);
+    let sum = num1 + num2;
+    document.getElementById('result').innerText = `The result is: ${sum}`;
+});
