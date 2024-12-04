0add03bdaae6cd2c8da310ff783d35e97893c090
LimeFrogg <bellamones@icloud.com>
2024-12-03 20:12:46 -0500

    Question 2

--- java.js
+++ java.js
@@ -2,3 +2,6 @@ function greetUser(Isabella) {
     document.getElementById('output').innerText = `Hello, ${Isabella}!`;
 }
 
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+    document.getElementById('message').innerText = 'Hello, World!';
+});
